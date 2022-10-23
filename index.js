168ee70e9d8af0f12335c00b6c127a1218e22ec9
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-10-23 17:44:44 -0400

    prod ready rest api - backend with dockerfile

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('./logger/logger');
+const app = express();
+const port = 3070;
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  logger.info('default route');
+  res.send('App works!!!!!');
+});
+
+app.use('/api', require('./routes/routes'));
+
+// request to handle undefined or all other routes
+app.get('*', function(req, res) {
+  logger.info('users route');
+  res.send('App works!!!!!');
+});
+
+app.listen(port, function(err) {
+  logger.info('running server on from port:::::::' + port);
+});
